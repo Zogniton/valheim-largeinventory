479b04622203a89ebd8401806b518196572e7213
Zogniton <Zogniton@gmail.com>
2021-02-23 17:27:50 +0200

    Initial commit

---
+++ LargeInventory.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using UnityEngine;
+namespace LargeInventory
+{
+    [BepInPlugin("org.bepinex.plugins.largeinventory", "Large Inventory", "1.0.0.0")]
+    public class LargeInventory : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Harmony harmony = new Harmony("mod.largeinventory");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Inventory), MethodType.Constructor)]
+    [HarmonyPatch(new Type[] { typeof(string), typeof(Sprite), typeof(int), typeof(int) })]
+    class Patch
+    {
+        static void Prefix(ref string name, ref Sprite bkg, ref int w, ref int h)
+        {
+            // Wood chest
+            if (h == 2 && w == 5)
+            {
+                h = 4;
+                w = 5;
+            }
+            // Player inventory
+            else if (h == 4 && w == 8)
+            {
+                h = 7;
+                w = 8;
+            }
+            // Iron chest, cart, boat
+            else if (h == 3 && w == 6)
+            {
+                h = 4;
+                w = 8;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(InventoryGui), "Show")]
+    public class InventoryGuiAwake
+    {
+        public static void Postfix(ref InventoryGui __instance, ref InventoryGrid ___m_playerGrid)
+        {
+            RectTransform container = __instance.m_container;
+            RectTransform player = __instance.m_player;
+
+            player.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 494);
+            container.offsetMax = new Vector2(610, -582);
+            container.offsetMin = new Vector2(40, -924);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Valheim Large Inventory
+This is a simple mod for the game Valheim, it increases player and storage chests inventory slots count.
+
+* Player inventory
+  * 7 x 8 for a total of 56 slots (default 32)
+* Wood chest
+  * 4 x 5 for a total of 20 slots (default 10)
+* Iron chest, Cart, Boats
+  * 4 x 8 for a total of 32 slots (default 18)
+
+![Demo](https://raw.githubusercontent.com/Zogniton/valheim-largeinventory/master/valheim-largeinventory.jpg)
+
+# Installation
+Requires BepInEx to load the plugin.
+Place LargeInventory.dll inside BepInEx/plugins folder.
+
+I personally use ValheimPlus mod which comes with BepInEx bundled and just add my dll to BepInEx/plugins folder.
+
+# Warning
+* When removing the mod, items in the removed slots will be lost
+* Use at your own risk
